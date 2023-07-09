fe6f08f14eefc55ab2322bff9571b481a0dadd1b
alejandro <alejandro.arafi@gmail.com>
2023-07-08 23:52:49 -0400

    proyecto terminado

--- src/Componentes/NavBar/NavBar.jsx
+++ src/Componentes/NavBar/NavBar.jsx
@@ -8,17 +8,10 @@ import './NavBar.css'
 export const NavBar = ({nombre='Home'}) => {
   return (
     
-    // <nav className='navbar'>
-    // <NavLink to="/">Home</NavLink>
-    // <NavLink to="/about">About</NavLink>
-    // <NavLink to="/menu">Menu</NavLink>
-    // <NavLink to="/reservas">Reservas</NavLink>
-    // </nav>
 
     <>
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
   <div className="container-fluid">
-    {/* <NavLink className="navbar-brand" to="/" ><img src="/docs/5.0/assets/brand/bootstrap-logo.svg" alt="" width="30" height="24"/></NavLink> */}
    
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
@@ -28,9 +21,7 @@ export const NavBar = ({nombre='Home'}) => {
         <li className="nav-item">
           <NavLink className="nav-link" to="/">{nombre}</NavLink>
         </li>
-        <li className="nav-item">
-          <NavLink className="nav-link" to="/about">About</NavLink>
-        </li>
+        
         <li className="nav-item">
           <NavLink className="nav-link" to="/menu" >
             Menu
