29114ca9caa1203a19bf4e45417856e4929e0fcc
alejandro <alejandro.arafi@gmail.com>
2023-07-08 21:54:10 -0400

    archivo readme listo

---
+++ README.md
@@ -0,0 +1,33 @@
+# Proyecto número 4 "Restaurant App"
+
+Este es el proyecto n°4 para el Bootcamp FullStack de la UDD, el cual fue desarrollado en React.
+
+## Descripción
+
+Este proyecto es una aplicación web para un restaurante italiano ubicado en la comuna de La Florida. La aplicación ofrece a los clientes una experiencia fácil y conveniente para explorar el menú del restaurante y realizar reservas de manera online. El objetivo principal es proporcionar a los usuarios una forma sencilla de interactuar con el restaurante y disfrutar de su cocina italiana.
+
+## Características
+
+- **Explorar el menú**: Los usuarios pueden navegar por el menú completo del restaurante, ver los platos disponibles, leer descripciones y precios.
+
+- **Reservas de mesas**: Los usuarios pueden reservar mesas en el restaurante seleccionando la fecha, hora y número de personas. La aplicación muestra la disponibilidad en tiempo real y confirma la reserva una vez completada.
+
+## Tecnologías Utilizadas
+
+- HTML
+- CSS
+- React (Hooks, props)
+- Bootstrap
+- Firebase
+- Vite
+
+## Instalación y Uso
+
+1. Clona este repositorio en tu máquina local:  (git clone https://github.com/tu-usuario/nombre-del-repositorio.git)
+2. Ejecuta `npm install` para instalar las dependencias necesarias.
+3. Configura la conexión con Firebase en el archivo de configuración (`src/config/firebase.js`) proporcionando tus propias credenciales.
+4. Ejecuta `npm start` para iniciar la aplicación en tu servidor local.
+5. Abre tu navegador web y accede a `http://localhost:3000` para ver la aplicación en funcionamiento.
+
+
+Espero que esta descripción te sea útil. Si tienes alguna otra pregunta, ¡no dudes en preguntar!
--- src/Pages/Menu/Menu.jsx
+++ src/Pages/Menu/Menu.jsx
@@ -6,366 +6,54 @@ export const Menu = () => {
   
   return (
     <>
+  <h1 className='titulo-container'>carta</h1>
+    
+  <div className="container-1">
 
-    {/* <h1 className='titulo-container'>carta</h1>
+<div class="row">
+  <div class="col">
+    <h1>entradas</h1>
+    <ul>
+    <li>Calamares a la romana</li>
+      <p> Calamares apanados y fritos</p>
 
-    <div className='menu-container'>
+      <li>Carpaccio Filete</li>
+      <p>Carpaccio de filete de res</p>
 
-      <div className='entradas-container'><h1>Entradas</h1>
+      <li>Carpaccio Salmón</li>
+      <p>Carpaccio de Salmón</p>
 
-        <h2>Calamares a la romana</h2>
-        <h3> Calamares apanados y fritos</h3>
+      <li>Gambas fritas y salsa Livornesa</li>
+      <p></p>
 
-        <h2>Carpaccio Filete</h2>
-        <h3>Carpaccio de filete de res</h3>
+      <li>Gamberi pil-pil</li>
+      <p>Camarones al pil-pil</p>
 
-        <h2>Carpaccio Salmón</h2>
-        <h3>Carpaccio de Salmón</h3>
+      <li>Belvedere</li>
+      <p>Mariscos en hojas de espinaca, salsa aioli</p>
 
-        <h2>Gambas fritas y salsa Livornesa</h2>
-        <h3></h3>
-
-        <h2>Gamberi pil-pil</h2>
-        <h3>Camarones al pil-pil</h3>
-
-        <h2>Belvedere</h2>
-        <h3>Mariscos en hojas de espinaca, salsa aioli</h3>
-
-        <h2>Caprese di sempre</h2>
-        <h3>Mozzarella de búfala en salsa de tomate tibia albahaca, rúcula y bruschettas</h3>
-        </div>
-
-        <div className='pastas-container'>
-    <h1>Pastas</h1>
-
-    <h2>SPAGHETTI POMODORO</h2>
-    <h3>Con tomate italiano natural y albahaca.</h3>
-
-    <h2>SPAGHETTI NAPOLITANA</h2>
-    <h3>CON SALSA DE TOMATES ARTESANAL LE DUE TORRI</h3>
-
-    <h2>SPAGHETTI AL PESTO</h2>
-    <h3>SPAGHETTI CON PESTO NATURAL ARTESANAL</h3>
-
-    <h2>SPAGHETTI PUTTANESCA</h2>
-    <h3>TOMATE ITALIANO, ANCHOAS, ALCAPARRAS, ACEITUNAS, UN LEVE TOQUE DE CACHO DE CABRA</h3>
-
+      <li>Caprese di sempre</li>
+      <p>Mozzarella de búfala en salsa de tomate tibia albahaca, rúcula y bruschettas</p>
+      </ul>
   </div>
-
-  <div className='carnes-container'>
-  <h1>Carnes</h1>
-  
-  <h2>LOMO VETADO A LA PARRILLA</h2>
-  <h3>LOMO A LA PARRILLA</h3>
-
-<h2>FILETE PAILLARD A LA PARRILLA</h2>
-<h3>Filete achurrascado con un poco de ajo y romero.</h3>
-
-<h2>ESCALOPINES AL VINO BLANCO</h2>
-<h3>EXQUISITOS ESCALOPINES AL VINO BLANCO
-</h3>
-
-<h2>ESCALOPINES AL MARSALA</h2>
-<h3>Suaves y tiernos cocinados lentamente y terminados en un maravilloso marsala italiano.</h3>
-
-<h2>LOMO VETADO EN SALSA FUNGHI</h2>
-<h3>LOMO SALSA FUNGHI</h3>
-
-</div>
-
-<div className='pescados-container' >
-  <h1>Pescados</h1>
-
-  <h2>SALMóN A LA PARRILLA</h2>
-  <h3>Salmón a la parrilla</h3>
-
-  <h2>MERLUZA AUSTRAL A LA PARRILLA</h2>
-
-  <h2>ALLA GENOVESE</h2>
-  <h3>Salmon, Merluza Austral o Reineta Con salsa de tomate cherry, orégano, aceitunas y alcaparra.</h3>
-
-<h2>CONGRIO A LA MEDITERRANEA</h2>
-<h3>Cocinado con Hierbas Frescas, Ajo, Zeste de limon, Oliva y Vino Blanco</h3>
-
-<h2>CONGRIO SALSA MARGARITA</h2>
-<h3>Congrio Grill con Salsa de Mariscos de la Temporada</h3>
-
-</div>
-
-<div className='acompañamientos-container'>
-  <h1>Acompañamientos</h1>
-
-<h2>ARROZ</h2>
-
-<h2>PAPAS FRITAS</h2>
-
-<h2>PAPAS SALTEADAS</h2>
-
-<h2>ENSALADA MIXTA</h2>
-
-<h2>PANACHE DE VERDURAS</h2>
-
-<h2>POLENTA</h2>
-
-<h2>PURE DE PAPAS</h2>
-
-<h2>PURE DE ESPINACAS</h2>
-
-<h2>CHAMPIÑONES SALTEADOS</h2>
-</div>
-
-<div className='vinos-container'>
-  <h1>VINOS</h1>
-
-  <h2>CASAS PATRONALES RESERVA</h2>
-  <h2>CHOCALAN RESERVA</h2>
-  <h2>SANTA EMA SELECT TERROIR RESERVA</h2>
-  <h2>MONTES ALPHA</h2>
-
-<h1>ESPUMANTES</h1>
-
-<h2>VIÑA MAR BRUT</h2>
-<h2>RICCARDONA ASTI</h2>
-<h2>CHARDON BRUT</h2>
-
-<div className='postres-container'>
-  <h1>POSTRES Y CAFES</h1>
-
-  <h2>COLORI DI STAGIONE</h2>
-  <h3>ENSALADA DE FRUTAS DE LA TEMPORADA</h3>
-
-  <h2>TIRAMISú CLáSICO</h2>
-  
-  <h2>FIOR DI LIMONE</h2>
-  <h3>Torta de panqueque y limón al estilo Due Torri.</h3>
-
-  <h2>PANNA COTTA DEL BOSCO</h2>
-  <h3>Con ragu de frutos del bosque.</h3>
-
-  <h2>VOLCANO DE CHOCOLATE</h2>
-  <h3>Volcan con deliciosa salsa de chocolate tibio y helado de vainilla.</h3>
-
-<h2>CHEESECAKE DUE TORRI</h2>
-<h3>Chessecake clasico con frutilla y salsa de nutella.</h3>
-
-<h2>TORTA FIRENZE</h2>
-<h3>Crema Inglesa, manjar, almendras y galletas de vino.</h3>
-
-<h2>BROWNIE E GELATTO</h2>
-<h3>Clásico bizcocho de chocolate con helado de vainilla, salsa de chocolate y nueces.</h3>
-
-</div>
-
-<div className='cafeteria-container'>
-  <h1>CAFETERIA-TETERIA</h1>
-
-  <h2>TE IMPORTADO</h2>
-
-  <h2>CAFÉ ESPRESSO</h2>
-
-  <h2>CORTADO</h2>
-
-  <h2>CAPUCCINO</h2>
-
-  </div>
-
-</div>
-
-</div> */}
-
-<h1 className='titulo-container'>carta</h1>
-
-
-
-
-<div className="container-1">
-
-  <div class="row">
-    <div class="col">
-      <h1>entradas</h1>
-      <ul>
-      <li>Calamares a la romana</li>
-        <p> Calamares apanados y fritos</p>
-
-        <li>Carpaccio Filete</li>
-        <p>Carpaccio de filete de res</p>
-
-        <li>Carpaccio Salmón</li>
-        <p>Carpaccio de Salmón</p>
-
-        <li>Gambas fritas y salsa Livornesa</li>
-        <p></p>
-
-        <li>Gamberi pil-pil</li>
-        <p>Camarones al pil-pil</p>
-
-        <li>Belvedere</li>
-        <p>Mariscos en hojas de espinaca, salsa aioli</p>
-
-        <li>Caprese di sempre</li>
-        <p>Mozzarella de búfala en salsa de tomate tibia albahaca, rúcula y bruschettas</p>
-        </ul>
-    </div>
 
 
 {/* pastas */}
-    <div class="col">
-      <h1>pastas</h1>
-      <li>SPAGHETTI POMODORO</li>
-    <p>Con tomate italiano natural y albahaca.</p>
+  <div class="col">
+    <h1>pastas</h1>
+    <li>SPAGHETTI POMODORO</li>
+  <p>Con tomate italiano natural y albahaca.</p>
 
-    <li>SPAGHETTI NAPOLITANA</li>
-    <p>CON SALSA DE TOMATES ARTESANAL LE DUE TORRI</p>
+  <li>SPAGHETTI NAPOLITANA</li>
+  <p>CON SALSA DE TOMATES ARTESANAL LE DUE TORRI</p>
 
-    <li>SPAGHETTI AL PESTO</li>
-    <p>SPAGHETTI CON PESTO NATURAL ARTESANAL</p>
+  <li>SPAGHETTI AL PESTO</li>
+  <p>SPAGHETTI CON PESTO NATURAL ARTESANAL</p>
 
-    <li>SPAGHETTI PUTTANESCA</li>
-    <p>TOMATE ITALIANO, ANCHOAS, ALCAPARRAS, ACEITUNAS, UN LEVE TOQUE DE CACHO DE CABRA</p>
-    </div>
+  <li>SPAGHETTI PUTTANESCA</li>
+  <p>TOMATE ITALIANO, ANCHOAS, ALCAPARRAS, ACEITUNAS, UN LEVE TOQUE DE CACHO DE CABRA</p>
   </div>
 </div>
-
-
-{/* nuevo container */}
-
-<div class="container">
-  <div class="row">
-    <div class="col">
-    <h1>Carnes</h1>
-  
-  <li>LOMO VETADO A LA PARRILLA</li>
-  <p>LOMO A LA PARRILLA</p>
-
-<li>FILETE PAILLARD A LA PARRILLA</li>
-<p>Filete achurrascado con un poco de ajo y romero.</p>
-
-<li>ESCALOPINES AL VINO BLANCO</li>
-<p>EXQUISITOS ESCALOPINES AL VINO BLANCO
-</p>
-
-<li>ESCALOPINES AL MARSALA</li>
-<p>Suaves y tiernos cocinados lentamente y terminados en un maravilloso marsala italiano.</p>
-
-<li>LOMO VETADO EN SALSA FUNGHI</li>
-<p>LOMO SALSA FUNGHI</p>
-    </div>
-    
-    {/* pescados */}
-    <div class="col">
-      <h1>Pescados</h1>
-
-  <li>SALMóN A LA PARRILLA</li>
-  <p>Salmón a la parrilla</p>
-
-  <li>MERLUZA AUSTRAL A LA PARRILLA</li>
-
-  <li>ALLA GENOVESE</li>
-  <p>Salmon, Merluza Austral o Reineta Con salsa de tomate cherry, orégano, aceitunas y alcaparra.</p>
-
-<li>CONGRIO A LA MEDITERRANEA</li>
-<p>Cocinado con Hierbas Frescas, Ajo, Zeste de limon, Oliva y Vino Blanco</p>
-
-<li>CONGRIO SALSA MARGARITA</li>
-<p>Congrio Grill con Salsa de Mariscos de la Temporada</p>
-    </div>
-  </div>
-</div>
-
-
-{/* nuevo container */}
-
-<div class="container">
-  <div class="row">
-    <div class="col">
-    <h2>ARROZ</h2>
-
-<li>PAPAS FRITAS</li>
-
-<li>PAPAS SALTEADAS</li>
-
-<li>ENSALADA MIXTA</li>
-
-<li>PANACHE DE VERDURAS</li>
-
-<li>POLENTA</li>
-
-<li>PURE DE PAPAS</li>
-
-<li>PURE DE ESPINACAS</li>
-
-<li>CHAMPIÑONES SALTEADOS</li>
-    </div>
-
-{/* vinos */}
-
-    <div class="col">
-    <h1>VINOS</h1>
-
-<li>CASAS PATRONALES RESERVA</li>
-<li>CHOCALAN RESERVA</li>
-<li>SANTA EMA SELECT TERROIR RESERVA</li>
-<li>MONTES ALPHA</li>
-
-<h1>ESPUMANTES</h1>
-
-<li>VIÑA MAR BRUT</li>
-<li>RICCARDONA ASTI</li>
-<li>CHARDON BRUT</li>
-    </div>
-    
-  </div>
-</div>
-
-
-
-{/* nuevo container */}
-
-<div class="container">
-  <div class="row">
-    <div class="col">
-    <h1>POSTRES Y CAFES</h1>
-
-<li>COLORI DI STAGIONE</li>
-<p>ENSALADA DE FRUTAS DE LA TEMPORADA</p>
-
-<li>TIRAMISú CLáSICO</li>
-
-<li>FIOR DI LIMONE</li>
-<p>Torta de panqueque y limón al estilo Due Torri.</p>
-
-<li>PANNA COTTA DEL BOSCO</li>
-<p>Con ragu de frutos del bosque.</p>
-
-<li>VOLCANO DE CHOCOLATE</li>
-<p>Volcan con deliciosa salsa de chocolate tibio y helado de vainilla.</p>
-
-<li>CHEESECAKE DUE TORRI</li>
-<p>Chessecake clasico con frutilla y salsa de nutella.</p>
-
-<li>TORTA FIRENZE</li>
-<p>Crema Inglesa, manjar, almendras y galletas de vino.</p>
-
-<li>BROWNIE E GELATTO</li>
-<p>Clásico bizcocho de chocolate con helado de vainilla, salsa de chocolate y nueces.</p>
-    </div>
-
-
-    {/* te y cafe */}
-    
-    <div class="col">
-    <h1>CAFETERIA-TETERIA</h1>
-
-<li>TE IMPORTADO</li>
-
-<li>CAFÉ ESPRESSO</li>
-
-<li>CORTADO</li>
-
-<li>CAPUCCINO</li>
-    </div>
-    
-  </div>
 </div>
 
 
